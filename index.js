f62c94aeff62d66723276d6bcbf5b9009b4a782f
This is Rude <dbauman1@swarthmore.edu>
2018-01-07 17:33:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -122,10 +122,11 @@ function endGame() {
 
 function moveDodger(e) {
      if (e.which===LEFT_ARROW){
-       e.preventDefault()
+       e.preventDefault();
+       e.stopPropagation();
        moveDodgerLeft();
      } else if (e.which ===RIGHT_ARROW){
-       e.preventDefault()
+       e.preventDefault();
        moveDodgerRight();
      }
    }
