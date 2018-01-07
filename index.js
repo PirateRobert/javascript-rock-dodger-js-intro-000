3d3ddde2d33f444b9b0f64e292a786708abb29ef
This is Rude <dbauman1@swarthmore.edu>
2018-01-07 17:40:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  $("div.rock").remove();
+  document.getElementById("div.rock").remove();
   window.removeEventListener('keydown', moveDodger);
   alert("YOU LOSE!")
 }
